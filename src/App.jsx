3c2b4d970f14e915d3f05d8364b595f463a60923
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-12-20 10:22:10 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./Components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <main className="h-screen my-8">
+        <Sidebar />
+      </main>
+    </>
+  );
+}
+
+export default App;
