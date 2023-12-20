b6a7bfecda06ba16cb003709e5199f31ce839d4f
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-12-20 12:33:04 +0300

    manage state to switch between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./Components/Sidebar";
 
 function App() {
+  const [projectState, SetProjectState] = useState({
+    projectSelectedId: undefined,
+    projects: [],
+  });
+
+  function handleProjectState() {
+    SetProjectState((prevState) => {
+      return {
+        ...prevState,
+        projectSelectedId: null,
+      };
+    });
+  }
+
+  let content;
+  if (projectState.projectSelectedId === null) {
+    content = <NewProject />;
+  } else {
+    content = <NoProjectSelected onProjectState={handleProjectState} />;
+  }
+
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <Sidebar />
-        <NoProjectSelected />
+        <Sidebar onProjectState={handleProjectState} />
+        {content}
       </main>
     </>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,13 @@
 import Button from "./Button";
 
-export default function Sidebar() {
+export default function Sidebar({ onProjectState }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-9 font-bold uppercase md:text-xl text-stone-200">
         Your Projects
       </h2>
 
-      <Button>+ Add Project</Button>
+      <Button onClick={onProjectState}>+ Add Project</Button>
       <ul></ul>
     </aside>
   );
