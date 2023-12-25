60fe8bffe82596a0d04efe0f10a3ff69f9ffe82e
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-12-25 11:17:02 +0300

    implement style for modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,6 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
+import Button from "./Button";
 
 const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
   const dialog = useRef();
@@ -13,10 +14,13 @@ const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
   });
 
   return createPortal(
-    <dialog ref={dialog}>
+    <dialog
+      ref={dialog}
+      className="backdrop:bg-stone-900/90 rounded-md p-4 shadow-md "
+    >
       {children}
-      <form method="dialog">
-        <button>{buttonCaption}</button>
+      <form method="dialog" className="mt-4 text-right">
+        <Button>{buttonCaption}</Button>
       </form>
     </dialog>,
     document.getElementById("modal-root")
